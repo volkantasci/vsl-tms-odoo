529b5fbf23dddb509b779211f4327b2e246a6d26
Volkan TAŞCI <me@volkantasci.com>
2026-05-07 22:09:02 +0300

    fix: use model_create_multi for Odoo 19 create override

--- vsl_tasimacilik/models/transport_order.py
+++ vsl_tasimacilik/models/transport_order.py
@@ -78,13 +78,14 @@ class VslTransportOrder(models.Model):
         required=True,
     )
 
-    @api.model
-    def create(self, vals):
-        if vals.get("name", _("New")) == _("New"):
-            vals["name"] = self.env["ir.sequence"].next_by_code(
-                "vsl.transport.order"
-            ) or _("New")
-        return super().create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if vals.get("name", _("New")) == _("New"):
+                vals["name"] = self.env["ir.sequence"].next_by_code(
+                    "vsl.transport.order"
+                ) or _("New")
+        return super().create(vals_list)
 
     def action_confirm(self):
         for order in self:
